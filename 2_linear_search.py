b97b4677287029f2fa7e70396e49ab8ea6f41386
SKShorna <shorna.sabira@gmail.com>
2020-12-22 23:30:29 -0500

    Simple linear search updated

--- 2_linear_search.py
+++ 2_linear_search.py
@@ -3,12 +3,23 @@
 
 num_list = [2, 4, 5, 7, 8]
 
-item = 5
+item = 2
+
+# flag 
+found = False
 
 for i in range(len(num_list)):
     if item == num_list[i]:
         print(i, end = " ")
         print(num_list[i])
-    
+        found = True
+
+if not found: # found == False
+   print("Item not found")
+
+
+
+
+  
 
     
